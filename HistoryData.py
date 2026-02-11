ed1e5fdeabb97e373f48b49263c8c587330c626a
nicomartinico <nidmartinezpe@unal.edu.co>
2026-02-10 23:03:55 -0500

    Llamada de datos - Prueba 1

--- HistoryData.py
+++ HistoryData.py
@@ -1,29 +1,40 @@
 import pandas as pd
 import soccerdata as scdat
 import os 
-
+from pathlib import Path
 sfscore = scdat.Sofascore()
+
 ligas = sfscore.read_leagues()
-print(ligas)
+# ligas.info()
+
+# print(ligas)
+
+
+sfscore = scdat.Sofascore("ENG-Premier League")
+temporadas = sfscore.read_seasons()
+# temporadas.info()
+
+# print(temporadas)
+
+sfscore = scdat.Sofascore("ENG-Premier League", ["2425","2526"])
+Posiciones = sfscore.read_league_table()
+# Posiciones.info()
+# print(Posiciones)
+
+sfscore = scdat.Sofascore("ENG-Premier League", ["2425","2526"])
+Calendario = sfscore.read_schedule()
+Calendario["date"] = pd.to_datetime(Calendario["date"], errors= "coerce").dt.tz_localize(None)
+# Calendario.info()
+# quit()
+# print(Calendario)
+
+path = r"C:\Users\USUARIO\Desktop\Futbol_xlsx"
+# ligas.to_excel(f"{path}\Ligas.xlsx", index= True)
+
+# temporadas.to_excel(f"{path}\Temporadas.xlsx", index= True)
+# Posiciones.to_excel(f"{path}\Posiciones.xlsx", index= True)
+Calendario.to_excel(f"{path}\Calendario.xlsx", index= True)
+
 quit()
 
 
-# Importar la clase
-sofascore = sd.Sofascore(leagues="ESP-La Liga", seasons="2022/2023")
-
-
-print("Class method")
-league_table = sofascore.read_league_table()
-league_table. info()
-league_table.head()
-
-
-schedule = sofascore.read_schedule()
-schedule.info()
-schedule.head()
-
-
-
-#ruta = r"C:\Users\USUARIO\Desktop\Futbol_xlsx\prueba.xlsx"
-#league_table.to_excel(ruta, index=False)
-#print("Descargo el excel en la ruta")
